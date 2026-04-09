dd561573ea04d26b388fbfe6395ad55cf875bc92
RubenAgra <rubenagra99@gmail.com>
2026-04-09 17:55:01 +0200

    refactor(config): extract EnableJpaAuditing from main class to JpaConfig class

---
+++ src/main/java/com/rubenac/saveslot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.rubenac.saveslot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
