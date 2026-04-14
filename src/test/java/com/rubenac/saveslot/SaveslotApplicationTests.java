b3f0e4b216d56e3576df951f9dfc267e0c381cce
RubenAgra <rubenagra99@gmail.com>
2026-04-14 11:51:14 +0200

    test(SaveSlot): add active test profile to SaveSlotApplicationTests

--- src/test/java/com/rubenac/saveslot/SaveslotApplicationTests.java
+++ src/test/java/com/rubenac/saveslot/SaveslotApplicationTests.java
@@ -2,10 +2,11 @@ package com.rubenac.saveslot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SaveslotApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
